70dcc6ff8c20d6b1fb520cadadf0fdd183138585
CHOI JEE HYUK <akfktl328@kyongi.ac.kr>
2024-05-10 03:23:37 +0900

    station 검색기능 개발

--- pages/🔎 search station.py
+++ pages/🔎 search station.py
@@ -1,13 +1,16 @@
+# core packages
 import streamlit as st
 import pandas as pd
 from utils import load_data_station
 from vis_function import showMapred
+
+
 def search_station(df, search_term):
     """검색어를 사용하여 station을 찾고 해당 정보를 반환하는 함수"""
     station_info = df[df['stationName'].str.contains(search_term)]
     return station_info
 
-# Streamlit 앱 구성
+
 def main():
     st.title('Station 검색기능')
     
@@ -15,11 +18,12 @@ def main():
     search_term = st.text_input('검색할 stationName을 입력하세요:')
     
     if search_term:
-        # 검색어를 사용하여 station 검색
+
         station_info = search_station(load_data_station(), search_term)
         station_infosrp = station_info[['stationName','rackTotCnt','parkingBikeTotCnt']]
+        
         if not station_infosrp.empty:
-            # 검색 결과 표시
+
             st.subheader('검색 결과:')
             st.write(station_infosrp)
         else:
